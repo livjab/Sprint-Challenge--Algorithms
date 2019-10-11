0c485ac95ee5a1ba4de5dbba886878424d98a58a
Liv Johnson <livjab@gmail.com>
2019-10-11 13:10:49 -0500

    still working through bugs on robot

--- robot_sort/robot_sort.py
+++ robot_sort/robot_sort.py
@@ -96,14 +96,61 @@ class SortingRobot:
         """
         Sort the robot's list.
         """
-        # Fill this out
-        pass
+        # I will try to implement something similar to bubble sort. This will
+        # allow the robot to reduce movements until it reaches the end of the list.
+
+        # Start by moving to the far right side of the list
+        while robot.can_move_right():
+            robot.move_right()
+        print(f"Position is {robot._position}")
+
+        # drop/swap "none" item with item in front of robot
+        robot.swap_item()
+        print(f"Holding item {robot._item}")
+
+        # Move to the far left side of list.
+        while robot.can_move_left():
+            robot.move_left()
+        print(f"Position is {robot._position}, and light on is {robot.light_is_on()}")
+
+        # Continue the below pattern until robot has reached the right side)
+        while robot.can_move_right():
+
+        # compare item in hand with item in front of robot
+        # If item in hand is lower, swap items, then move one space to right
+        # If a swap occurs, turn on the robot's light.
+            if robot.compare_item() == -1:
+                robot.swap_item()
+                robot.set_light_on()
+                robot.move_right()
+
+            # If item in hand is higher, keep it and move one space to the right
+            elif robot.compare_item() == 1:
+                robot.move_right()
+
+
+            # If items are equal, keep and move one space to right.
+            elif robot.compare_item() == 0:
+                robot.move_right()
+
+        # When robot reaches 'none' item at far right side ...
+        if robot.compare_item() == None:
+            # Check if light is on, if so return to far left, reset light to off
+            if robot.light_is_on():
+                robot.set_light_off()
+                while robot.can_move_left():
+                    robot.move_left()
+
+            # If light is off, swap item in hand for "none" and return list
+            elif robot.light_is_off():
+                robot.swap_item()
+                #return robot._list
 
 
 if __name__ == "__main__":
     # Test our your implementation from the command line
     # with `python robot_sort.py`
-
+    #l = [5, 4, 3, 2, 1]
     l = [15, 41, 58, 49, 26, 4, 28, 8, 61, 60, 65, 21, 78, 14, 35, 90, 54, 5, 0, 87, 82, 96, 43, 92, 62, 97, 69, 94, 99, 93, 76, 47, 2, 88, 51, 40, 95, 6, 23, 81, 30, 19, 25, 91, 18, 68, 71, 9, 66, 1, 45, 33, 3, 72, 16, 85, 27, 59, 64, 39, 32, 24, 38, 84, 44, 80, 11, 73, 42, 20, 10, 29, 22, 98, 17, 48, 52, 67, 53, 74, 77, 37, 63, 31, 7, 75, 36, 89, 70, 34, 79, 83, 13, 57, 86, 12, 56, 50, 55, 46]
 
     robot = SortingRobot(l)
